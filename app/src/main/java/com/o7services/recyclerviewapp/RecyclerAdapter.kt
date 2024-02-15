709d22e3568e051ba891d9ed206cdb275974821a
Amanpreet Kaur <69182131+amanpreeto7@users.noreply.github.com>
2024-02-15 15:26:42 +0530

    Initial commit

---
+++ app/src/main/java/com/o7services/recyclerviewapp/RecyclerAdapter.kt
@@ -0,0 +1,37 @@
+package com.o7services.recyclerviewapp
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+/**
+ * @Author: 017
+ * @Date: 15/02/24
+ * @Time: 11:37 am
+ */
+class RecyclerAdapter(var recyclerClickInterface: RecyclerClickInterface) : RecyclerView.Adapter<RecyclerAdapter.ViewHolderClass>() {
+    inner class ViewHolderClass(var view: View): RecyclerView.ViewHolder(view){
+        var tvText = view.findViewById<TextView>(R.id.tvName)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolderClass {
+        var view = LayoutInflater.from(parent.context).inflate(R.layout.item_adapter, parent, false)
+        return ViewHolderClass(view)
+    }
+
+    override fun getItemCount(): Int {
+        return 10
+    }
+
+    override fun onBindViewHolder(holder: ViewHolderClass, position: Int) {
+        holder.tvText.setText("Text $position")
+        holder.tvText.setOnClickListener {
+            recyclerClickInterface.onItemClick()
+        }
+        //data set
+        //click listener
+    }
+
+}
---
+++ app/src/main/java/com/o7services/recyclerviewapp/RecyclerClickInterface.kt
@@ -0,0 +1,10 @@
+package com.o7services.recyclerviewapp
+
+/**
+ * @Author: 017
+ * @Date: 15/02/24
+ * @Time: 12:20 pm
+ */
+interface RecyclerClickInterface {
+    fun onItemClick()
+}
